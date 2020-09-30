c3d1cccbcb0b4fc71c739a802c080547280bd117
syedwshah <syedwshah.nyc@gmail.com>
2020-09-30 16:13:30 -0400

    Create a custom React-Navigation TabBar

--- mobile/src/constants/images.js
+++ mobile/src/constants/images.js
@@ -1,4 +1,19 @@
 export const images = {
-    logo: require('../../assets/img/brandLogo.png'),
-    googleColorIcon: require('../../assets/img/googleIcon.png')
+	logo: require("../../assets/img/brandLogo.png"),
+	googleColorIcon: require("../../assets/img/googleIcon.png"),
+};
+
+export const tabBarIcons = {
+	active: {
+		Home: require("../../assets/all/tabIcon/HomeIconActive.png"),
+		List: require("../../assets/all/tabIcon/ListIconActive.png"),
+		Stores: require("../../assets/all/tabIcon/StoresIconActive.png"),
+		Order: require("../../assets/all/tabIcon/OrderIconActive.png"),
+	},
+	inactive: {
+		Home: require("../../assets/all/tabIcon/HomeIconInactive.png"),
+		List: require("../../assets/all/tabIcon/ListIconInactive.png"),
+		Stores: require("../../assets/all/tabIcon/StoresIconInactive.png"),
+		Order: require("../../assets/all/tabIcon/OrderIconInactive.png"),
+	},
 };
